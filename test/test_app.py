fe847f2053b1a7d39c5a9476247270960593628e
Ivana <ivana.zaccheddu88@gmail.com>
2020-02-05 11:44:39 +0100

    add json

--- test/test_app.py
+++ test/test_app.py
@@ -1,3 +1,15 @@
 def test_home(client):
     r=client.get('/')
     r.status == '200'
+
+def test_get(client):
+    a=client.get('/api/v1.0/users')
+    a.status == '200'
+
+def test_get1(client):
+    b=client.get('/api/v1.0/users/1')
+    b.status == '200'
+
+def test_get2(client):
+    c=client.get('/api/v1.0/users/?user_id=1')
+    c.status == '200'
